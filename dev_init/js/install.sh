a447163e9a019bf2bb44cc0c8253bf5da2594ae7
jenix21 <devfrog@gmail.com>
2015-04-05 19:56:02 +0900

    added dev init

---
+++ dev_init/js/install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# ruby required.
+gem install sass
+gem install compass:1.0.3
+gem install compass-sourcemaps --pre
+
+npm install -g yo
+npm install -g bower
+npm install -g grunt-cli
---
+++ dev_init/js/setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+npm install -g generator-angular // angular generator install
+
+yo angular # scaffolding
+grunt # build
+grunt server # run local server
